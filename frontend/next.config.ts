2bd1ef3b72f24e1513b1ed228124da520500b4f8
George Lugo <machinelearner2334@gmail.com>
2025-10-10 19:24:48 +0000

    fix: added proper build traces

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // ...add any other Next.js config here
 };
 
 export default nextConfig;
